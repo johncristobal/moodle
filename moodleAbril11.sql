daeb886850eed42545c06c17524108807cb8490e
LAPTOP-S5BK4FJV\place <carlosmayarodriguez@gmail.com>
2019-04-12 11:35:26 +0200

    Base de datos con relación usuario-alumno

--- moodleAbril11.sql
+++ moodleAbril11.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 09-04-2019 a las 21:16:45
--- Versión del servidor: 10.1.21-MariaDB
--- Versión de PHP: 5.6.30
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-04-2019 a las 11:33:22
+-- Versión del servidor: 10.1.37-MariaDB
+-- Versión de PHP: 7.1.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -31,15 +33,17 @@ CREATE TABLE `alumnos` (
   `nombre` varchar(100) NOT NULL,
   `edad` int(11) NOT NULL,
   `estatus` int(11) NOT NULL,
-  `fecha_alta` date NOT NULL
+  `fecha_alta` date NOT NULL,
+  `Fk_usuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `alumnos`
 --
 
-INSERT INTO `alumnos` (`id`, `nombre`, `edad`, `estatus`, `fecha_alta`) VALUES
-(1, 'Carlos', 30, 1, '2019-04-10');
+INSERT INTO `alumnos` (`id`, `nombre`, `edad`, `estatus`, `fecha_alta`, `Fk_usuario`) VALUES
+(1, 'Carlos', 30, 1, '2019-04-10', 1),
+(2, 'Brina Molko', 24, 1, '2019-04-12', 10);
 
 -- --------------------------------------------------------
 
@@ -62,7 +66,8 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`id`, `correo`, `password`, `estatus`, `rol`, `fecha_alta`) VALUES
 (1, 'nowoscmexico@gmail.com', 'nowoscadm19', 1, 1, '2019-04-09'),
-(2, 'cristobaljohn00@gmail.com', 'cristobal12345', 1, 1, '2019-04-09');
+(2, 'cristobaljohn00@gmail.com', 'cristobal12345', 1, 1, '2019-04-09'),
+(10, 'carlosmayarodriguez@gmail.com', 'asds', 1, 3, '2019-04-12');
 
 --
 -- Índices para tablas volcadas
@@ -72,7 +77,8 @@ INSERT INTO `usuarios` (`id`, `correo`, `password`, `estatus`, `rol`, `fecha_alt
 -- Indices de la tabla `alumnos`
 --
 ALTER TABLE `alumnos`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Fk_usuario` (`Fk_usuario`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -88,12 +94,25 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `alumnos`
 --
 ALTER TABLE `alumnos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD CONSTRAINT `alumnos_ibfk_1` FOREIGN KEY (`Fk_usuario`) REFERENCES `usuarios` (`id`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
