015e68a20f586ae45a8d8c31321324287eebe899
LAPTOP-S5BK4FJV\place <carlosmayarodriguez@gmail.com>
2019-05-16 12:06:53 +0200

    Adding footer in all pages.

---
+++ application/views/footer.php
@@ -0,0 +1,82 @@
+	
+
+	<!-- Footer -->
+
+	<footer class="footer">
+		<div class="container">
+			<div class="row">
+
+				<!-- About -->
+				<div class="col-lg-3 footer_col">
+					<div class="footer_about">
+						<div class="logo_container">
+							<a href="#">
+								<div class="logo_content d-flex flex-row align-items-end justify-content-start">
+									<div class="logo_img"><img class="logo" src="<?= base_url() ?>images/logo_instituto.jpg" alt=""></div>
+									
+								</div>
+							</a>
+						</div>
+						<div class="footer_about_text">
+							<br>
+							<p>Institución Educativa con 32 años de experiencia y con 17 Planteles en la Ciudad de México y el área Metropolitana</p>
+						</div>
+						<div class="footer_social">
+							<ul>
+								<li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+								<li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
+								<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+							</ul>
+						</div>-
+						<div class="copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> Derechos reservados | <a href="#" target="_blank">Nowosc México</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></div>
+					</div>
+				</div>
+
+				<div class="col-lg-3 footer_col">
+					<div class="footer_links">
+						<div class="footer_title">Oferta educativa</div>
+						<ul class="footer_list">
+							<li><a href="#">Derecho</a></li>
+							<li><a href="#">Contaduría</a></li>
+							<li><a href="#">Oferta educativa</a></li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-lg-3 footer_col">
+					<div class="footer_links">
+						<div class="footer_title">Nosotros</div>
+						<ul class="footer_list">
+							<li><a href="#">¿Quiénes somos?</a></li>
+							<li><a href="#">Misión</a></li>
+							<li><a href="#">Visión</a></li>
+							<li><a href="#">Preguntas frecuentes</a></li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-lg-3 footer_col">
+					<div class="footer_contact">
+						<div class="footer_title">Contacto</div>
+						<div class="footer_contact_info">
+							<div class="footer_contact_item">
+								<div class="footer_contact_title">Ubicación:</div>
+								<div class="footer_contact_line">Calle falsa 123, Tultepec, Estado de México </div>
+							</div>
+							<div class="footer_contact_item">
+								<div class="footer_contact_title">Teléfono:</div>
+								<div class="footer_contact_line">+53 345 7953 32453</div>
+							</div>
+							<div class="footer_contact_item">
+								<div class="footer_contact_title">Correo electrónico:</div>
+								<div class="footer_contact_line">idea@gmail.com</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
